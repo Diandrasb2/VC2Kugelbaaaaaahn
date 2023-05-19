90b6e7d00ff1491a00008dd0b93beffa08db4d92
Diandra <92119418+Diandrasb2@users.noreply.github.com>
2023-05-19 15:16:33 +0200

    .

--- src/gui/KugelbahnController.java
+++ src/gui/KugelbahnController.java
@@ -61,19 +61,30 @@ public class KugelbahnController {
 		StartVLabel.setText(StartVValue + " m/s");
 	}
 
+	double vx;
+	double vy;
+	
+	double sx;
+	double sy;
+	
 	//Kugel bewegt sich nach unten bis zum Boden
-	public void movement(GraphicsContext graphicsContext,double gravityValue) {
-		for(int i = 1; i <= 100; i++) {
-			v = StartVSlider.getValue();
-			v = v + gravityValue * dT;
-			s = s + v * dT + 0.5 * gravityValue * Math.pow(dT, 2);	// Strecke S
+	public void movement(GraphicsContext graphicsContext, double gravityValue) {
+		vx = StartVSlider.getValue();
+		vy = StartVSlider.getValue();
+		//for(int i = 1; i <= 500; i++) {  //ohne Schleife bewegt sich die Kugel, aber sehr langsam weil der denkt dass der Screen 650000+ Meter groß ist wegen 1000er Faktor
+			checkCollision(Kugel, Ebene1);
+			vx = vx + 0 * dT;
+			vy = vy - gravityValue * dT;
+			sx = sx + vx * dT + 0.5 * gravityValue * Math.pow(dT, 2);	// Strecke S
+			sy = sy + vy * dT + 0.5 * gravityValue * Math.pow(dT, 2);
 			if(Kugel.getLayoutY() >= 614) {
 				System.out.println("Boden");
-				v = -v;
+				vy = -vy;
 			}else{
-				Kugel.setLayoutY(s);
+				Kugel.setLayoutX(sx);
+				Kugel.setLayoutY(sy);
 			}
-		}
+		//}
 	}
 	// Startknopf betätigen --> Kugel fängt an sich zu bewegen und man kann sie verschieben
 	@FXML
@@ -82,6 +93,7 @@ public class KugelbahnController {
 		canvas = new Canvas(750, 650);
 		double gravityValue = GravitySlider.getValue();
 		System.out.println("Button pressed.");
+
 
 		GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 		Timeline timeline = new Timeline(new KeyFrame(Duration.millis(dT*1000),e -> movement(graphicsContext, gravityValue)));
@@ -105,6 +117,14 @@ public class KugelbahnController {
 			node.setLayoutX(e.getSceneX() - startX);
 			node.setLayoutY(e.getSceneY() - startY);
 		});
+	}
+
+
+	public void checkCollision(Circle Kugel, Rectangle Ebene1) {
+		if(Kugel.getBoundsInParent().intersects(Ebene1.getBoundsInParent())) {
+			System.out.println("Kollision");
+
+		}
 	}
 
 }
